9a601413f6c8ad894d50ffb157237e9f28e465b5
Abhinav Mall <abhinav.0.mall@gmail.com>
2019-07-18 21:33:46 +0530

    First commit

---
+++ src/main/java/com/assignment/egmat/EgmatApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.egmat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgmatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgmatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/egmat/EgmatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.assignment.egmat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EgmatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
