326abf4020dbbc561394d528be666d023913e82b
Abhinav Mall <abhinav.0.mall@gmail.com>
2019-07-19 23:56:51 +0530

    Upload file working with validations

--- src/main/java/com/assignment/controller/QuestionFileUploadController.java
+++ src/main/java/com/assignment/controller/QuestionFileUploadController.java
@@ -1,11 +1,21 @@
 package com.assignment.controller;
 
 import com.assignment.dto.form.UploadFileForm;
+import com.assignment.service.FileService;
+import com.assignment.util.Utilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.io.File;
 
 @Controller
 public class QuestionFileUploadController {
@@ -17,11 +27,61 @@ public class QuestionFileUploadController {
     private static final String FILE_UPLOAD_PAGE =
             "uploadquestionfile";
 
-    @GetMapping("/upload")
+
+    @Autowired
+    Utilities utilities;
+
+    @Autowired
+    FileService fileService;
+
+    @GetMapping("/uploadquestionfile")
     public String getQuestionFileUploadPage(
             Model model) {
         logger.info("Getting file upload page");
         model.addAttribute("uploadFileForm", new UploadFileForm());
         return FILE_UPLOAD_PAGE;
+    }
+
+    @PostMapping("/uploadquestionfile")
+    public String getQuestionPaperCountFromFile(
+            @ModelAttribute("uploadFileForm")
+            @Valid
+                    UploadFileForm uploadFileForm,
+            BindingResult bindingResult,
+            Model model,
+            RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            logger.error("No file uploaded");
+            model.addAttribute("failure", "Invalid file");
+            return FILE_UPLOAD_PAGE;
+        }
+
+        // Check File type
+        if (null == uploadFileForm.getFile()
+                || uploadFileForm.getFile().isEmpty()
+                || !utilities.isTextFile(
+                uploadFileForm.getFile())) {
+            bindingResult.rejectValue("file",
+                    null,
+                    "Please select a valid text file.");
+            logger.error("Invalid file uploaded");
+            model.addAttribute("failure", "Invalid file");
+            return FILE_UPLOAD_PAGE;
+        }
+
+        File file =
+                fileService.convertMultipartFileToFile(
+                        uploadFileForm.getFile());
+
+        // TODO: Count and populate redirect attributes
+        int count = 0;
+
+        redirectAttributes.addFlashAttribute("success",
+                "Total number of question papers possible = "
+                        + count
+                        + " from file "
+                        + uploadFileForm.getFile().getOriginalFilename());
+
+        return "redirect:/" + FILE_UPLOAD_PAGE;
     }
 }
---
+++ src/main/java/com/assignment/util/Utilities.java
@@ -0,0 +1,40 @@
+package com.assignment.util;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class Utilities {
+    private static final Logger logger =
+            LoggerFactory
+                    .getLogger(
+                            Utilities.class);
+
+    private static final String TEXT_FILE_EXTENSION = "txt";
+
+    public boolean isTextFile(MultipartFile multipartFile) {
+        boolean result = false;
+        if (null == multipartFile
+                || multipartFile.isEmpty()) {
+            logger.info("File is empty. Returning false.");
+        }
+
+        String extension = FilenameUtils
+                .getExtension(
+                        multipartFile.getOriginalFilename());
+
+        if (StringUtils
+                .equalsIgnoreCase(
+                        extension,
+                        TEXT_FILE_EXTENSION)) {
+            logger.info("File is text file.");
+            result = true;
+        }
+
+        return result;
+    }
+}
